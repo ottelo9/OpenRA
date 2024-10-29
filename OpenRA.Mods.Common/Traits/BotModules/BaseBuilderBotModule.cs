8d3c0ee9bb38f8a98eb1aa46ef5e2e2ff208d0b5
ottelo <33122175+ottelo9@users.noreply.github.com>
2024-10-29 20:20:58 +0100

    Added support for bots protecting structures with walls
    From here https://github.com/OpenRA/OpenRA/pull/19853

--- OpenRA.Mods.Common/Traits/BotModules/BaseBuilderBotModule.cs
+++ OpenRA.Mods.Common/Traits/BotModules/BaseBuilderBotModule.cs
@@ -53,6 +53,12 @@ namespace OpenRA.Mods.Common.Traits
 		[Desc("Production queues AI uses for defenses.")]
 		public readonly HashSet<string> DefenseQueues = new() { "Defense" };
 
+		[Desc("Tells the AI what building types are considered barriers.")]
+		public readonly HashSet<string> WallTypes = new();
+
+		[Desc("Buildings that should be walled off, if any.")]
+		public readonly HashSet<string> WalledStructures = new();
+
 		[Desc("Minimum distance in cells from center of the base when checking for building placement.")]
 		public readonly int MinBaseRadius = 2;
 
@@ -334,7 +340,7 @@ namespace OpenRA.Mods.Common.Traits
 
 			if (possibleRallyPoints.Count == 0)
 			{
-				AIUtils.BotDebug("{0} has no possible rallypoint near {1}", producer.Owner, producer.Location);
+				AIUtils.BotDebug($"{producer.Owner} has no possible rallypoint near {producer.Location}");
 				return producer.Location;
 			}
 
